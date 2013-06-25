b00884c9dae61fad11eeeedb03fa3d830a8d4e57
Udo Waechter <u.waechter@mysportgroup.de>
2013-06-25 18:02:27 +0200

    create a link to /var/lib/jenkins

--- manifests/jetty.pp
+++ manifests/jetty.pp
@@ -1,12 +1,18 @@
-class jenkins::jetty ($ensure = 'present') inherits jenkins::params {
+class jenkins::jetty (
+  $ensure = 'present') inherits jenkins::params {
   if !defined(Class['jetty']) {
     err("${hostname} also must use Class[jetty]")
   }
+
   Jenkins::Params {
     user    => 'jetty',
     group   => 'jetty',
     service => 'jetty'
   }
-  #http://mirrors.jenkins-ci.org/war/latest/jenkins.war
-  #download and install
+
+  # http://mirrors.jenkins-ci.org/war/latest/jenkins.war
+  # download and install
+  if $ensure == 'present' {
+    file { '/usr/share/jetty/.jenkins': ensure => '/var/lib/jenkins', }
+  }
 }
