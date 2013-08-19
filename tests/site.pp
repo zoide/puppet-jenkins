afa5972d48a9236d1a9eb975948f9dd6728a3a5b
Udo Waechter <u.waechter@mysportgroup.de>
2013-08-19 18:06:48 +0200

    removing defautl node, this would break doc generation

--- tests/site.pp
+++ tests/site.pp
@@ -1,14 +1,14 @@
-node default {
-  # Classify all nodes with the site specific jenkins class.
-  class { 'jenkins': }
-  group {
-    'puppet' :
-      ensure => present;
-  }
-
-  jenkins::plugin {
-    'git' : ;
-  }
-}
-
-# EOF
+#node default {
+#  # Classify all nodes with the site specific jenkins class.
+#  class { 'jenkins': }
+#  group {
+#    'puppet' :
+#      ensure => present;
+#  }
+#
+#  jenkins::plugin {
+#    'git' : ;
+#  }
+#}
+#
+## EOF
